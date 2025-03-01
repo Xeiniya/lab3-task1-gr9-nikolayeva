cad997ba9117c497fa21f9475797cd379420e1df
Kseniya <xeiniya@gmail.com>
2025-03-01 09:12:34 +0000

    Добавлена функция вычисления суммы цифр числа

--- .ccls-cache/@home@runner@workspace/src@main.c
+++ .ccls-cache/@home@runner@workspace/src@main.c
@@ -1,10 +1,19 @@
-/* hello.c */
+/* main.c */
 #include <stdio.h>
+
+/* Функция для вычисления суммы цифр числа */
+int sumOfDigits(int n) {
+	int sum = 0;
+	while (n > 0) {
+		sum += n % 10;
+		n /= 10;
+	}
+	return sum;
+}
 
 int main (void)
 {
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
-
+	int test_number = 12345;
+	printf("Сумма цифр числа %d: %d\n", test_number, sumOfDigits(test_number));
 	return 0;
 }
